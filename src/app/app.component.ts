c24db0f79e5dedb21f4ccbfaf9caa12af514e9cc
GABRIEL FELLIPE MARQUARDT <gabriel_marquardt@estudante.sesisenai.org.br>
2023-05-03 16:36:14 -0300

    Aprimoramento no css e adição da função remover em cada uma das tarefas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface Tarefa{
+  id: number
+  nome: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  nome: string = '';
+  tarefas: Tarefa[] = []
+  proximoId = 1;
+
+  tarefa={
+    nome: ''
+  }
+
+  cadastrarTarefa(): void { 
+    const novaTarefa: Tarefa = {
+      id: this.proximoId,
+      nome: this.tarefa.nome
+    };
+    this.tarefas.push(novaTarefa);
+    this.proximoId++;
+  }
+
+  removerTarefa(id: number): void {
+    this.tarefas = this.tarefas.filter(tarefa => tarefa.id !== id);
+  }
+
 }
