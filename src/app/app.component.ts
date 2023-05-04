cb006d1c3711e54eecced5b9aca56784f34fe7e7
GABRIEL FELLIPE MARQUARDT <gabriel_marquardt@estudante.sesisenai.org.br>
2023-05-04 14:59:09 -0300

    Mais alterações no css, adição do localStorage e mais algumas pequenas mudanças no HTML/TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 interface Tarefa{
   id: number
   nome: string
+
 }
 @Component({
   selector: 'app-root',
@@ -26,10 +27,23 @@ export class AppComponent {
     };
     this.tarefas.push(novaTarefa);
     this.proximoId++;
+    localStorage.setItem('tarefas', JSON.stringify(this.tarefas));
   }
+  
 
   removerTarefa(id: number): void {
     this.tarefas = this.tarefas.filter(tarefa => tarefa.id !== id);
+  
+    localStorage.setItem('tarefas', JSON.stringify(this.tarefas));
+  }
+  
+
+  ngOnInit() {
+    const tarefasSalvas = localStorage.getItem('tarefas');
+    if (tarefasSalvas) {
+      this.tarefas = JSON.parse(tarefasSalvas);
+      this.proximoId = this.tarefas[this.tarefas.length - 1].id + 1;
+    }
   }
 
 }
